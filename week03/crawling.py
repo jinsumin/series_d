a9a37ec0f590e6554f87d599003b8327d2763eb3
Yeojin Kim <bluemeta@gmail.com>
2021-05-01 15:38:20 +0900

    네이버 영화 사이트 제목 크롤링

--- week03/crawling.py
+++ week03/crawling.py
@@ -7,8 +7,19 @@ response = requests.get(
     headers=headers
 )
 
-print(response.text)
+# print(response.text)
 
 # HTML 데이터 가공
 soup = BeautifulSoup(
     response.text, 'html.parser')
+
+
+selector = '#old_content > table > tbody > tr'
+title_selector = 'td.title > div > a'
+titles = soup.select(selector)
+
+for title in titles:
+    title_tag = title.select_one(title_selector)
+
+    if title_tag:
+        print(title_tag.text)
